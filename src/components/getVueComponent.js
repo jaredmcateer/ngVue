9c0ca4b92436b8ab42faf2a3827c6937b1967188
Jared McAteer <jared.mcateer@gmail.com>
2022-04-21 11:20:35 -0600

    fix: injector getting composition api should wrap in vue.component

--- src/components/getVueComponent.js
+++ src/components/getVueComponent.js
@@ -3,10 +3,15 @@ import Vue from 'vue'
 import { isCompositionApi } from '../../lib/isCompositionApi'
 
 export default function getVueComponent(component, $injector) {
+  if (angular.isString(component)) {
+    component = $injector.get(component)
+  }
+
   if (angular.isFunction(component)) {
     return component
   } else if (isCompositionApi(component)) {
     return Vue.component(component.name || 'UnnamedComponent', component)
   }
-  return $injector.get(component)
+
+  return component
 }
